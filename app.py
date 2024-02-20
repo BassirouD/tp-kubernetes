8d9cb4df83b63ebe0fd64bc4f590748901783a5f
bassiroutdsi <bassiroutds@gmail.com>
2024-02-20 11:12:14 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def motd():
+    message = os.getenv('MESSAGE')
+    if not message:
+        return jsonify({'error': 'MESSAGE environment variable is not set'}), 500
+
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    message = os.getenv('MESSAGE')
+    port = os.getenv('APP_PORT', 5000)
+    if not message:
+        print("MESSAGE environment variable is not set.")
+    else:
+        print("Starting motd-api with message:", message)
+        app.run(debug=True, port=int(port))
